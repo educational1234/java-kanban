c3ac1fa223cc6de932af725416b6a1bae6229f83
3agent999 <agentsmith999@yandex.ru>
2024-07-12 16:21:49 +0300

    Исправлены стилистические ошибки в TaskManager.java

--- src/main/managers/TaskManager.java
+++ src/main/managers/TaskManager.java
@@ -8,25 +8,41 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     int createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTaskById(int id);
 
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     int createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
 
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     Subtask getSubtaskById(int id);
+
     int createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtaskById(int id);
+
     List<Subtask> getSubtasksByEpicId(int epicId);
 
     List<Task> getHistory();
